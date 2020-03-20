401c50289157d486103bdcd015392b47cf95ab0b
Florian Tischler <florian.tischler@gmx.net>
2020-03-20 21:50:45 +0100

    Add heroku deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,12 +6,17 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
+
+  app.useStaticAssets(join(__dirname, '..', 'homeboi'));
+
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
