24dc1ae930272258270a21ca1b0d2a6a826736b8
Marco <marcohaemmerle@gmx.de>
2020-03-21 15:38:06 +0100

    Add getall bookings and hand back booking

--- apps/api/src/app/bookings/booking.controller.ts
+++ apps/api/src/app/bookings/booking.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingPostDto } from './dto/booking.post.dto';
 import { User } from '../shared/user.decorator';
 import { UserEntity } from '../entities/user.entity';
+import { BookingEntity } from '../entities/booking.entity';
 
 @Controller('bookings')
 export class BookingController {
@@ -10,9 +11,22 @@ export class BookingController {
   constructor(private readonly bookingsService: BookingService) {
   }
 
+  @Get('')
+  async getAllBookings(): Promise<BookingEntity[]> {
+    return this.bookingsService.getAllBookings();
+  }
+
   @Post('')
   async createNewBooking(@Body() createBookingDto: BookingPostDto, @User() user: UserEntity) {
     return this.bookingsService.createNewBooking(createBookingDto, user);
+  }
+
+  @Put('hand-back/:id')
+  async handBackBooking(@Param('id') bookingId: string): Promise<void> {
+    const success = await this.bookingsService.handBackBooking(bookingId);
+    if (!success) {
+      throw new NotFoundException();
+    }
   }
 
   @Delete(':id')
