b5c668859fe650bc06e13d30c79691fcc2e6813e
Marco Hämmerle <38943824+mhaemmerle42@users.noreply.github.com>
2020-03-22 11:53:29 +0100

    Fix booking end date (#61)

--- apps/api/src/app/entities/booking.entity.ts
+++ apps/api/src/app/entities/booking.entity.ts
@@ -13,7 +13,7 @@ export class BookingEntity implements Omit<Booking, 'product'> {
   @Column({ type: 'date' })
   start: Date;
 
-  @Column({ type: 'date' })
+  @Column({ type: 'date', nullable: true })
   end: Date;
 
   @ManyToOne(type => ProductEntity, type => type.bookings)
